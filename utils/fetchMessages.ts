f3a89a8c857e3c12d9963c53a3cef36e19b8d3ab
Ohad Green <osahar5@gmail.com>
2022-12-17 12:39:56 +0200

    added vercel url

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -2,7 +2,7 @@ import { MessageProps } from '../app/ChatInput';
 
 export const fetcher = async () => {
   const url = process.env.VERCEL_URL || 'http://localhost:3000';
-  const result = await fetch(`${url}api/getMessages`);
+  const result = await fetch(`${url}/api/getMessages`);
   const data = await result.json();
   const messages: MessageProps[] = data.messages;
   return messages;
