a869c9afeb6bc033fc34daa304d5fb18c6fe56d7
Ohad Green <osahar5@gmail.com>
2022-12-17 19:43:02 +0200

    api url fix

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 export default function Head() {
   return (
-    <div>
+    <React.Fragment>
       <title>Meta messenger</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </div>
-  )
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <link rel='icon' href='/favicon.ico' />
+    </React.Fragment>
+  );
 }
